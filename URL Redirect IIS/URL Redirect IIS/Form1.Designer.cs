19a65f190ffaa703c20497a7644da4411ededcda
KrunalSoni3006 <kunalsoni00@gmail.com>
2017-05-22 09:16:39 +0530

    add new project with GUI

---
+++ URL Redirect IIS/URL Redirect IIS/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace URL_Redirect_IIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectslnFile = new System.Windows.Forms.Button();
+            this.btnSelectFromURls = new System.Windows.Forms.Button();
+            this.btnStartURLRedirect = new System.Windows.Forms.Button();
+            this.btnSelectToUrls = new System.Windows.Forms.Button();
+            this.lblselectsln = new System.Windows.Forms.Label();
+            this.lblSelectFromURLs = new System.Windows.Forms.Label();
+            this.lblSelectToURLs = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectslnFile
+            // 
+            this.btnSelectslnFile.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectslnFile.Name = "btnSelectslnFile";
+            this.btnSelectslnFile.Size = new System.Drawing.Size(126, 23);
+            this.btnSelectslnFile.TabIndex = 0;
+            this.btnSelectslnFile.Text = "Select Project .sln FIle";
+            this.btnSelectslnFile.UseVisualStyleBackColor = true;
+            this.btnSelectslnFile.Click += new System.EventHandler(this.btnSelectslnFile_Click);
+            // 
+            // btnSelectFromURls
+            // 
+            this.btnSelectFromURls.Location = new System.Drawing.Point(12, 41);
+            this.btnSelectFromURls.Name = "btnSelectFromURls";
+            this.btnSelectFromURls.Size = new System.Drawing.Size(126, 23);
+            this.btnSelectFromURls.TabIndex = 1;
+            this.btnSelectFromURls.Text = "Select From URLs";
+            this.btnSelectFromURls.UseVisualStyleBackColor = true;
+            this.btnSelectFromURls.Click += new System.EventHandler(this.btnSelectFromURls_Click);
+            // 
+            // btnStartURLRedirect
+            // 
+            this.btnStartURLRedirect.Location = new System.Drawing.Point(12, 99);
+            this.btnStartURLRedirect.Name = "btnStartURLRedirect";
+            this.btnStartURLRedirect.Size = new System.Drawing.Size(569, 23);
+            this.btnStartURLRedirect.TabIndex = 2;
+            this.btnStartURLRedirect.Text = "Start URL Redirect";
+            this.btnStartURLRedirect.UseVisualStyleBackColor = true;
+            this.btnStartURLRedirect.Click += new System.EventHandler(this.btnStartURLRedirect_Click);
+            // 
+            // btnSelectToUrls
+            // 
+            this.btnSelectToUrls.Location = new System.Drawing.Point(12, 70);
+            this.btnSelectToUrls.Name = "btnSelectToUrls";
+            this.btnSelectToUrls.Size = new System.Drawing.Size(126, 23);
+            this.btnSelectToUrls.TabIndex = 3;
+            this.btnSelectToUrls.Text = "Select To URLs";
+            this.btnSelectToUrls.UseVisualStyleBackColor = true;
+            this.btnSelectToUrls.Click += new System.EventHandler(this.btnSelectToUrls_Click);
+            // 
+            // lblselectsln
+            // 
+            this.lblselectsln.AutoSize = true;
+            this.lblselectsln.Location = new System.Drawing.Point(148, 17);
+            this.lblselectsln.Name = "lblselectsln";
+            this.lblselectsln.Size = new System.Drawing.Size(0, 13);
+            this.lblselectsln.TabIndex = 4;
+            // 
+            // lblSelectFromURLs
+            // 
+            this.lblSelectFromURLs.AutoSize = true;
+            this.lblSelectFromURLs.Location = new System.Drawing.Point(148, 46);
+            this.lblSelectFromURLs.Name = "lblSelectFromURLs";
+            this.lblSelectFromURLs.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectFromURLs.TabIndex = 5;
+            // 
+            // lblSelectToURLs
+            // 
+            this.lblSelectToURLs.AutoSize = true;
+            this.lblSelectToURLs.Location = new System.Drawing.Point(148, 75);
+            this.lblSelectToURLs.Name = "lblSelectToURLs";
+            this.lblSelectToURLs.Size = new System.Drawing.Size(0, 13);
+            this.lblSelectToURLs.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 311);
+            this.Controls.Add(this.lblSelectToURLs);
+            this.Controls.Add(this.lblSelectFromURLs);
+            this.Controls.Add(this.lblselectsln);
+            this.Controls.Add(this.btnSelectToUrls);
+            this.Controls.Add(this.btnStartURLRedirect);
+            this.Controls.Add(this.btnSelectFromURls);
+            this.Controls.Add(this.btnSelectslnFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectslnFile;
+        private System.Windows.Forms.Button btnSelectFromURls;
+        private System.Windows.Forms.Button btnStartURLRedirect;
+        private System.Windows.Forms.Button btnSelectToUrls;
+        private System.Windows.Forms.Label lblselectsln;
+        private System.Windows.Forms.Label lblSelectFromURLs;
+        private System.Windows.Forms.Label lblSelectToURLs;
+    }
+}
+
---
+++ URL Redirect IIS/URL Redirect IIS/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace URL_Redirect_IIS
+{
+    public partial class Form1 : Form
+    {
+        public string ProjectSourcePath { get; private set; }
+        public string FromURLs { get; private set; }
+        public string ToURLs { get; private set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectslnFile_Click(object sender, EventArgs e)
+        {
+            ProjectSourcePath = OpenfileDialog("Project Solution File|*.sln", "Select .sln File Of Project");
+            lblselectsln.Text = ProjectSourcePath;
+        }
+
+        private void btnSelectFromURls_Click(object sender, EventArgs e)
+        {
+            FromURLs = OpenfileDialog("From URL .txt File|*.txt", "Select From URLs File");
+            lblSelectFromURLs.Text = FromURLs;
+        }
+
+        private void btnSelectToUrls_Click(object sender, EventArgs e)
+        {
+            ToURLs = OpenfileDialog("To URL .txt  File|*.txt", "Select To URLs File");
+            lblSelectToURLs.Text = ToURLs;
+        }
+
+        private void btnStartURLRedirect_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string OpenfileDialog(string filter, string title)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = filter;
+            dialog.Title = title;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                return dialog.FileName;
+            }
+            return "";
+        }
+    }
+}
